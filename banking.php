9f121376be325cf47b77e55c6592674e0d5fc356
Aldric Gaudinot <gaudinot@gmail.com>
2017-03-14 23:56:47 +0100

    fix typo

--- banking.php
+++ banking.php
@@ -13,14 +13,14 @@ Version: 1.6
 
 include(realpath(dirname(__FILE__)) . "/widgets/ranking.widget.php");
 include(realpath(dirname(__FILE__)) . "/widgets/bankbanner.widget.php");
-include(realpath(dirname(__FILE__)) . "/widgets/BankHierarchy.widget.php");
+include(realpath(dirname(__FILE__)) . "/widgets/bankHierarchy.widget.php");
 include(realpath(dirname(__FILE__)) . "/shortcodes/bankranking.php");
 include(realpath(dirname(__FILE__)) . "/shortcodes/proscons.php");
 include(realpath(dirname(__FILE__)) ."/class/BankReviewJson.php");
 
 function banking_plugin_enqueue_styles()
 {
-    $template_directory = plugin_dir_url( __FILE__ );
+    $template_directory = plugin_dir_url(__FILE__);
     wp_register_script('star-rating-js', $template_directory.'/star-rating/js/star-rating.min.js', array('jquery'), null, true);
     wp_register_style('star-rating-css', $template_directory.'/star-rating/css/star-rating.min.css', false, null, 'all');
     wp_enqueue_script('star-rating-js');
@@ -28,5 +28,3 @@ function banking_plugin_enqueue_styles()
 }
 
 add_action('wp_enqueue_scripts', 'banking_plugin_enqueue_styles', 15);
-
-?>
