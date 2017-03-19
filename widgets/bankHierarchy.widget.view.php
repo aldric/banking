83205dc06a74db2a6248a6e4f476209e86ad1fb1
Aldric Gaudinot <gaudinot@gmail.com>
2017-03-19 19:38:13 +0100

    widget

--- widgets/bankHierarchy.widget.view.php
+++ widgets/bankHierarchy.widget.view.php
@@ -1,29 +1,30 @@
 <div>
   <h3 class="widget-title">
-    <span>Catalogue 2do</span>
+    <span>Catalogue</span>
 </h3>
   <div class="widget-custom">
-      <?php 
+      <?php
       global $post;
       $current_post_id = $post->ID;
-      echo '<ul class="nav nav-pills  flex-column">'.printList($model, $current_post_id).'</ul>'; ?>
+      echo '<ul class="">'.printList($model, $current_post_id).'</ul>'; ?>
  </div>
 </div>
-<?php 
+<?php
 //class="active"
-  function printList($row, $p_id) {
-    $active = $row->id == $p_id ? 'class="active"' : '';
-    $out = '<li class="nav-item"><a href="'.$row->permalink.'" '.$active.'>'.$row->title.'</a>';
+  function printList($row, $p_id)
+  {
+      $active = $row->id == $p_id ? 'class="active"' : '';
+      $out = '<li class=""><a href="'.$row->permalink.'" '.$active.'>'.$row->title.'</a>';
 
-    if(count($row->children) > 0) {
-      $out .= '<ul class="nav flex-column">';
-      foreach($row->children as $child) {
-        $out .= printList($child, $p_id);
+      if (count($row->children) > 0) {
+          $out .= '<ul class="">';
+          foreach ($row->children as $child) {
+              $out .= printList($child, $p_id);
+          }
+          $out .= '</ul>';
+      } else {
+          $out .= '</li>';
       }
-      $out .= '</ul>';
-    } else {
-      $out .= '</li>';
-    }
     //$out .= '</ul>';
     return $out;
   }
