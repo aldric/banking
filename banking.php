db4faebc82124304daec8a1829de4aea6924592d
a.gaudinot <gaudinot@gmail.com>
2017-04-04 16:43:47 +0200

    carousel OK

--- banking.php
+++ banking.php
@@ -15,6 +15,7 @@ include(realpath(dirname(__FILE__)) . "/widgets/ranking.widget.php");
 include(realpath(dirname(__FILE__)) . "/widgets/bankbanner.widget.php");
 include(realpath(dirname(__FILE__)) . "/widgets/bankHierarchy.widget.php");
 include(realpath(dirname(__FILE__)) . "/shortcodes/bankranking.php");
+include(realpath(dirname(__FILE__)) . "/shortcodes/bankcarousel.php");
 include(realpath(dirname(__FILE__)) . "/shortcodes/proscons.php");
 include(realpath(dirname(__FILE__)) ."/class/BankReviewJson.php");
 
