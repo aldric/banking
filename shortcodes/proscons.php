1fbb8fdd2544330b72da3ffd9628ede16d40f45f
aldric <gaudinot@gmail.com>
2017-05-02 22:35:55 +0200

    add proscons shortcodes

--- shortcodes/proscons.php
+++ shortcodes/proscons.php
@@ -1,59 +1,44 @@
 <?php
 
-//[proscons]
+class ProsConsModel
+{
+    public $pros = array();
+    public $cons = array();
+    public $pros_title;
+    public $cons_title;
+    public function __construct($pros_title = 'Points positifs',
+                                $cons_title = 'Points négatifs',
+                                $pros = array(),
+                                $cons = array())
+    {
+        $this->pros_title = $pros_title;
+        $this->cons_title = $cons_title;
+        $this->pros = $pros;
+        $this->cons = $cons;
+    }
+}
+//[proscons bank="fortuneo"]
 function proscons_func()
 {
-    $post_object = get_field('associated_bank');
+    $a = shortcode_atts(array(
+      'bank' => '',
+      'pros_title' => 'Points positifs',
+      'cons_title' => 'Points négatifs',
+  ), $atts);
 
-    $pros = array();
-    $cons = array();
+    $repo = new Bank_Repository();
+    $bank_input = $a['bank'];
+    $bank = explode('|', $bank_input);
 
-    if ($post_object != null) {
-        $id = $post_object->ID;
-        $name = get_field('bank_name_label', $id);
-        echo 'plop : '.$name;
-        if(have_rows('points_forts', $id)) {
-            while(have_rows('points_forts', $id)) {
-                the_row();
-                array_push($pros, get_sub_field('point_fort', $id));
-            }
-        }
-        
-        if(have_rows('points_faibles', $id)) {
-            while(have_rows('points_faibles', $id)) {
-                the_row();
-                array_push($cons, get_sub_field('point_faible', $id));
-           }
-        }
-    }
-    $class = count($pros) > 0 && count($cons) > 0 ? 'col-md-6' : 'col-md-12';
-    $out = "<div class=\"container-fluid\"><div class=\"row\">";
+    $data = $repo->get_banks_data($bank);
+    $out = ViewRenderer::render('proscons.html', new ProsConsModel(
+      $a['pros_title'],
+      $a['cons_title'],
+      $data->pros,
+      $data->cons
+    ));
 
-    $iterator1 = new ArrayIterator($pros);
-    $iterator2 = new ArrayIterator($cons);
-    $multiple = new MultipleIterator(MultipleIterator::MIT_NEED_ANY);
-    $multiple->attachIterator($iterator1);
-    $multiple->attachIterator($iterator2);
-   
-   if(count($pros) > 0 ) {
-       $out .= '<div class="'.$class.' bg-success pc-tab"><h3>Points forts</h3></div>';
-   }
-   if(count($cons) > 0 ) {
-       $out .= '<div class="'.$class.' bg-warning pc-tab"><h3>Points faibles</h3></div>';
-   }
-
-    foreach ($multiple as $value) {
-        $key = $multiple->key();
-        $pro = $key[0];
-        $out .= "<div class=\"row\">";
-        $out .= '<div class="'.$class.'">'.$value[0].'</div>';
-        $out .= '<div class="'.$class.'">'.$value[1].'</div>';
-        $out .= '</div>';
-    }
-    $out .= "</div></div>";
     return $out;
 }
 
 add_shortcode('proscons', 'proscons_func');
-
-?>
