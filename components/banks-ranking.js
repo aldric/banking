6a9170fed2e4c62879d69fdbfc8a3a62a4899a4a
Aldric Gaudinot <gaudinot@gmail.com>
2017-07-03 22:55:31 +0200

    Cleanup html markup
    Remove useless th / compute width on the fly / compute number of visible
    cols on the fly

--- components/banks-ranking.js
+++ components/banks-ranking.js
@@ -17,69 +17,89 @@ var bankRankingComponent = Vue.component('banks-grid', {
         offer: Array
     },
     data: function() {
-        var checkedNames = [];
-        this.banks.forEach(function(bank, idx) {
-          if(idx < 3)
-            checkedNames.push(bank.name);
-        });
         this.offer.sort();
         return {
-            checkedNames: checkedNames,
+            checkedNames: [],
             mobileApps: [],
             selectedServices: []
         }
     },
-    beforeUpdate: function () {
+    beforeUpdate: function() {
 
     },
-    updated: function () {
-      jQuery('button[data-type=opinion]').popover({ content : function () {
-        return jQuery(this).prev().html();
-      }});
+    updated: function() {
+        jQuery('button[data-type=opinion]').popover({
+            content: function() {
+                return jQuery(this).prev().html();
+            }
+        });
 
-      jQuery('button[data-type=opinion]').popover({ content : function () {
-        return jQuery(this).prev().html();
-      }});
+        jQuery('button[data-type=opinion]').popover({
+            content: function() {
+                return jQuery(this).prev().html();
+            }
+        });
     },
     mounted: function() {
-      var that = this;
-      jQuery('button[data-type=result]').popover({  html : true, content : function () {
-        var id = jQuery(this).attr('id');
-        return that.getSearchDetails(id);
+        var that = this;
+        this.$nextTick(function() {
+            window.addEventListener('resize', this.setCheckedNames);
+            this.setCheckedNames();
+        });
+        jQuery('button[data-type=result]').popover({
+            html: true,
+            content: function() {
+                var id = jQuery(this).attr('id');
+                return that.getSearchDetails(id);
 
-      }});
-      jQuery('button[data-type=result]').popover({ html : true, content : function () {
-        return jQuery(this).prev().html();
-        return that.getSearchDetails(id);
-      }});
+            }
+        });
+        jQuery('button[data-type=result]').popover({
+            html: true,
+            content: function() {
+                return jQuery(this).prev().html();
+                return that.getSearchDetails(id);
+            }
+        });
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.setCheckedNames);
     },
     methods: {
-      getSearchDetails : function(bankId) {
-          var that = this;
-          var bank = this.filteredBanks.find(function(bank) {
-            return bank.name == bankId;
-          });
-          var apps = this.mobileAppScore(bank);
-          var services = this.serviceScore(bank);
-          var result = "<h5>Note de la rédaction</h5><div style=\"text-align:center;font-weight:bold;margin-bottom:10px\">" + that.getNote(bank.mean) + "</div>";
-          if(services.services.length > 0) {
-            result += "<h5>Services disponibles</h5>";
-            result += "<ul>";
-            services.services.forEach(function(s) {
-                result += "<li>" + s + "</li>";
+        setCheckedNames: function() {
+            var that = this;
+            that.checkedNames.splice(0);
+            this.banks.forEach(function(bank, idx) {
+                if (idx < (that.numberOfBanks > 0 ? that.numberOfBanks : 3))
+                    that.checkedNames.push(bank.name);
             });
-            result += "</ul>";
-          }
-          if(apps.mobileApps.length > 0) {
-            result += "<h5>Disponible sur</h5>";
-            result += "<ul>";
-            apps.mobileApps.forEach(function(s) {
-                result += "<li><i class=\"" + that.getMobileIcon(s) + "\" aria-hidden=\"true\"></i></li>";
+        },
+        getSearchDetails: function(bankId) {
+            var that = this;
+            var bank = this.filteredBanks.find(function(bank) {
+                return bank.name == bankId;
             });
-            result += "</ul>";
-          }
-          return result;
-      },
+            var apps = this.mobileAppScore(bank);
+            var services = this.serviceScore(bank);
+            var result = "<h5>Note de la rédaction</h5><div style=\"text-align:center;font-weight:bold;margin-bottom:10px\">" + that.getNote(bank.mean) + "</div>";
+            if (services.services.length > 0) {
+                result += "<h5>Services disponibles</h5>";
+                result += "<ul>";
+                services.services.forEach(function(s) {
+                    result += "<li>" + s + "</li>";
+                });
+                result += "</ul>";
+            }
+            if (apps.mobileApps.length > 0) {
+                result += "<h5>Disponible sur</h5>";
+                result += "<ul>";
+                apps.mobileApps.forEach(function(s) {
+                    result += "<li><i class=\"" + that.getMobileIcon(s) + "\" aria-hidden=\"true\"></i></li>";
+                });
+                result += "</ul>";
+            }
+            return result;
+        },
         getNote: function(note) {
             var n = (note / 10);
             return (Math.round(n) == n ? n : n.toFixed(1)) + '/10';
@@ -102,19 +122,25 @@ var bankRankingComponent = Vue.component('banks-grid', {
             var mobileApps = [];
             for (var i in bank.mobile_apps) {
                 if (bank.mobile_apps[i] === true && this.mobileApps.indexOf(i) > -1) {
-                  mobileApps.push(i);
+                    mobileApps.push(i);
                 }
             }
-            return { score : mobileApps.length, mobileApps : mobileApps };
+            return {
+                score: mobileApps.length,
+                mobileApps: mobileApps
+            };
         },
         serviceScore: function(bank) {
             var services = [];
             for (var i in bank.offer) {
                 if (bank.offer[i] === "1" && this.selectedServices.indexOf(i) > -1) {
-                  services.push(i);
+                    services.push(i);
                 }
             }
-            return { score : services.length, services : services };
+            return {
+                score: services.length,
+                services: services
+            };
         },
         resetFilters: function() {
             this.selectedServices.splice(0);
@@ -123,69 +149,72 @@ var bankRankingComponent = Vue.component('banks-grid', {
 
     },
     computed: {
-      numberOfBanks : function() {
-        var nb = this.banks.length;
-        if(screen.width < 415) {
-          nb = 3;
-        } else if(screen.width < 601) {
-          nb = 4;
-        }  else if(screen.width < 1025) {
-          nb = 5;
-        }
-        var value = Math.min(this.banks.length, nb);
-        return value;
-      },
+        columnWidth: function() {
+            return "width: " + (100 / this.filteredBanks.length) + "%";
+        },
+        numberOfBanks: function() {
+            var nb = this.banks.length;
+            if (screen.width < 415) {
+                nb = 3;
+            } else if (screen.width < 601) {
+                nb = 4;
+            } else if (screen.width < 1025) {
+                nb = 5;
+            }
+            var value = Math.min(this.banks.length, nb);
+            return value;
+        },
         searchStarted: function() {
-          return (this.selectedServices && this.selectedServices.length > 0) || (this.mobileApps && this.mobileApps.length > 0);
+            return (this.selectedServices && this.selectedServices.length > 0) || (this.mobileApps && this.mobileApps.length > 0);
         },
         filteredBanks: function() {
             var that = this;
             return this.banks.filter(function(bank) {
                 return that.checkedNames.indexOf(bank.name) > -1;
             }).sort(function(a, b) {
-              return a.mean < b.mean;
+                return a.mean < b.mean;
             });
         },
         topBanks: function() {
-          var topBanks = this.headers.slice().sort(function(a, b) {
-            return a.score < b.score;
-          }).sort(function(a, b) {
-              if(a.score == b.score)
-                return a.mean < b.mean;
-              return 0;
-          });
-          var rewards = [ 'gold-medal', 'silver-medal', 'bronze-medal'];
-          var idx = 0;
-          topBanks.forEach(function(bank, i) {
-            bank.reward = rewards[idx];
-            bank.rank = idx + 1;
-            if(topBanks[i + 1] == undefined) return;
-            var nextBank = topBanks[i + 1];
-            if(bank.score != nextBank.score || bank.mean != nextBank.mean)
-              idx++;
-          });
-          return topBanks;
+            var topBanks = this.headers.slice().sort(function(a, b) {
+                return a.score < b.score;
+            }).sort(function(a, b) {
+                if (a.score == b.score)
+                    return a.mean < b.mean;
+                return 0;
+            });
+            var rewards = ['gold-medal', 'silver-medal', 'bronze-medal'];
+            var idx = 0;
+            topBanks.forEach(function(bank, i) {
+                bank.reward = rewards[idx];
+                bank.rank = idx + 1;
+                if (topBanks[i + 1] == undefined) return;
+                var nextBank = topBanks[i + 1];
+                if (bank.score != nextBank.score || bank.mean != nextBank.mean)
+                    idx++;
+            });
+            return topBanks;
         },
         headers: function() {
             var b = this.filteredBanks;
             var headers = [];
             var that = this;
             b.forEach(function(bank) {
-               var apps = that.mobileAppScore(bank);
-               var services = that.serviceScore(bank);
+                var apps = that.mobileAppScore(bank);
+                var services = that.serviceScore(bank);
                 headers.push({
                     name: bank.name,
                     mean: bank.mean,
                     image: bank.icon,
                     icon: bank.favicon,
                     score: apps.score + services.score,
-                    apps : apps.mobileApps,
-                    services : services.services,
+                    apps: apps.mobileApps,
+                    services: services.services,
                     reward: undefined,
                     rank: undefined
                 });
             });
-          return headers;
+            return headers;
         },
         evaluations: function() {
             var b = this.filteredBanks;
